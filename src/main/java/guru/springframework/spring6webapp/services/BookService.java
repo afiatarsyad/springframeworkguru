85d58ba5503409cff228a734e1147c2c1b58713c
afiatarsyad <afiat.arsyad@gmail.com>
2025-06-26 17:45:31 +0700

    init commit

--- src/main/java/guru/springframework/spring6webapp/services/BookService.java
+++ src/main/java/guru/springframework/spring6webapp/services/BookService.java
@@ -1,9 +1,7 @@
 package guru.springframework.spring6webapp.services;
 
-import guru.springframework.spring6webapp.domain.Book;
-import org.springframework.stereotype.Service;
+import guru.springframework.spring6webapp.domain.Book;import org.springframework.stereotype.Service;
 
-@Service
 public interface BookService {
 
     Iterable<Book> findAll();
